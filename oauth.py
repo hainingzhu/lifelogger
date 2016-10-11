8c3d67c8f62120fba911fc25ce3d73e3eaafd10d
Hongjian <hongjian-wang@outlook.com>
2016-10-11 12:49:12 -0400

    Adding `moves` APP oauth process

---
+++ oauth.py
@@ -0,0 +1,33 @@
+"""
+Hongjian
+10/10/2016
+
+Oauth script for Moves app.
+"""
+
+
+import flask_oauthlib.client import OAuth
+
+
+def get_moves_oauth_credential():
+    credentials = {}
+    with open("oauth_credentials") as fin:
+        credentials["moves_client_id"] = fin.readline().strip()
+        credentials["moves_client_secret"] = fin.readline().strip()
+    return credentials
+    
+    
+def moves_oauth_server(app=None):
+    credentials = get_moves_oauth_credential()
+    
+    oauth = OAuth(app)
+    moves = oauth.remote_app(
+        name="moves",
+        consumer_key=credentials["moves_client_id"],
+        consumer_secret=credentials["moves_client_secret"],
+        authorize_url="https://api.moves-app.com/oauth/v1/authorize",
+        request_token_params={'scope':'activity heartrate',
+                              'expires_in': 2592000},
+        access_token_method="POST",
+        access_token_url="
+        )
