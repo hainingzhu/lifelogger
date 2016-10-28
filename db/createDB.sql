fd9cb4a8e1a200c7d883d41b658ec9f4174e38a7
Hongjian <hongjian-wang@outlook.com>
2016-10-28 00:08:50 -0400

    query fitbit data / save fitbit Oauth into local DB (#12, #8)

--- db/createDB.sql
+++ db/createDB.sql
@@ -17,3 +17,11 @@ CREATE TABLE IF NOT EXISTS rescuetime (
 	uid INTEGER REFERENCES users(userid),
 	rescuetime_token text
 );
+
+
+CREATE TABLE IF NOT EXISTS fitbit (
+    uid INTEGER REFERENCES users(userid),
+    fitbit_token text,
+    fitbit_user_id text,
+    fitbit_refresh_token text
+);
