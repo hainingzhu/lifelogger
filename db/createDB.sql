ebf56d0a0885d3626bccedf251521b2e41fad1e9
MariaChen08 <jwchen.maria@gmail.com>
2016-11-07 16:55:08 -0500

    add create table for manual tracking survey and entry

--- db/createDB.sql
+++ db/createDB.sql
@@ -25,3 +25,30 @@ CREATE TABLE IF NOT EXISTS fitbit (
     fitbit_user_id text,
     fitbit_refresh_token text
 );
+
+CREATE TABLE IF NOT EXISTS manual_track_survey (
+	id INTEGER PRIMARY KEY,
+	uid INTEGER REFERENCES users(userid),
+	submit_date date,
+	question_step int,
+	question_alcohol text,
+	question_cigarette text,
+	question_sleep int,
+	question_work text,
+	question_leisure text,
+	question_maintenance text,
+	question_other_type text,
+	question_other text
+);
+
+CREATE TABLE IF NOT EXISTS manual_track_entry (
+	id INTEGER PRIMARY KEY,
+	survey_id INTEGER REFERENCES manual_track_survey(id),
+	what_happen text,
+	code text,
+	time_began text,
+	time_end text,
+	where_happen text,
+	feel text,
+	stree text
+);
